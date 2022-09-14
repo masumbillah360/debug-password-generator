6aec8bdc43c9025ce732e54e64dc9078432e47b5
masumbillah360 <masumbillah6584@gmail.com>
2022-09-14 09:19:55 +0600

    fixed show password on input field

--- script.js
+++ script.js
@@ -30,18 +30,18 @@ const handleCheckbox = () => {
 };
 
 const generatePassword = () => {
-  const passwordEl = getElement("pass");
+  const passwordEl = getElement("password");
 
-  const charCount = getElement("char-count");
+  const charCount = getElement("char-count").innerText;
 
   let password = "";
 
-  for (let i = 0; i < charCount.length; i++) {
+  for (let i = 0; i < charCount; i++) {
     const randomNumber = Math.floor(Math.random() * charSet.length);
     password += charSet.substring(randomNumber, randomNumber + 1);
   }
-
-  passwordEl.innerText = password;
+console.log(password)
+  passwordEl.value = password;
 };
 
 const handleViewPassword = () => {
